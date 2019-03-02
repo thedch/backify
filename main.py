7b080b944d1d500317f26abb073513eb024f059b
Daniel Hunter <officialdanielhunter@gmail.com>
2019-03-02 15:55:44 -0800

    Updated playlists at March_02_2019_15:55:44

--- main.py
+++ main.py
@@ -9,13 +9,22 @@ os.environ['SPOTIPY_CLIENT_SECRET'] = CLIENT_SECRET
 os.environ['SPOTIPY_REDIRECT_URI'] = REDIRECT_URI
 
 
-def print_tracks(tracks:dict, f):
+def write_tracks(tracks:dict, f):
     for track in tracks['items']:
-        track = track['track'] # painful json nesting
-        f.write(track['name'] + ' (by) ' + track['artists'][0]['name'] + '\n')
+        track = track['track']
+        if 'is_playable' in track:
+            if track['is_playable']:
+                flag = ''
+            else:
+                flag = ' !! NOT PLAYABLE'
+        else:
+            flag = ' !! PLAYABLE UNKNOWN'
+
+        f.write(track['name'] + ' (by) ' + track['artists'][0]['name'] + flag + '\n')
 
 
 scope = 'playlist-read-private'
+market='from_token'
 token = util.prompt_for_user_token(username, scope)
 
 sp = spotipy.Spotify(auth=token)
@@ -25,14 +34,15 @@ playlists = sp.current_user_playlists()
 for name, id in playlist_ids.items():
     print(f'Processing playlist {name}')
     with open(f'playlists/{name}.txt', 'w+') as f:
-        results = sp.user_playlist(username, id)
-        tracks = results['tracks']
-        print_tracks(tracks, f)
+        tracks = sp.user_playlist_tracks(username, id, market=market)
+        write_tracks(tracks, f)
         while tracks['next']:
             tracks = sp.next(tracks)
-            print_tracks(tracks, f)
+            write_tracks(tracks, f)
 
-# grab saved tracks as well
-saved_tracks = sp.current_user_saved_tracks(limit=50)
+# grab saved tracks as well -- 'market' is not a supported keyword of
+# current_user_saved_tracks(), you must edit the library source code
+# for this to run
+saved_tracks = sp.current_user_saved_tracks(limit=50, market=market)
 with open(f'playlists/saved_tracks.txt', 'w+') as f:
-    print_tracks(saved_tracks, f)
+    write_tracks(saved_tracks, f)
