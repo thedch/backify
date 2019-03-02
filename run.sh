342f40a9216a83bfb68f35189be942abc31c39ca
Daniel Hunter <officialdanielhunter@gmail.com>
2019-03-02 10:54:55 -0800

    Fix errors in run script

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
 
 python main.py
 git add --all
 datetime=$(date "+%B_%d_%Y_%T")
-git commit -m "Updated playlists at $(datetime)"
+git commit -m "Updated playlists at ${datetime}"
 git push
